97583cc95766b98aa3679c9018d3549f4f99157a
webdevrithms <divicenzi.webdev@gmail.com>
2022-04-05 23:52:43 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.nav-principal', {
+    distance: '20px',
+    duration: 2500
+});
+
+sr.reveal('.listado-categorias', {
+    duration: 2500,
+    origin: 'top',
+    distance: '-100px'
+});
+
+sr.reveal('.producto', {
+    distance: '200px',
+    duration: 2000
+});
+
+//  Nosotros 
+sr.reveal('.contenido-nosotros', {
+    distance: '200px',
+    origin: 'right',
+    duration: 2500
+});
+
+sr.reveal('.img-galeria', {
+    distance: '200px',
+    origin: 'bottom',
+    duration: 2000
+});
+
+
